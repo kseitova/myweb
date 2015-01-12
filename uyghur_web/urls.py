2646ad16af00c97e256547a48ec13a31cbae43b9
Alex Kudlick <alex@rescale.com>
2015-01-11 16:22:07 -0800

    Coalesce into one app

---
+++ uyghur_web/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+import uyghur_web.views
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'uyghur_web.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', uyghur_web.views.index, name='index'),
+    url(r'^suggestion/$', uyghur_web.views.suggestion, name='suggestion'),
+    url(r'^create_suggestion/$', uyghur_web.views.create_suggestion, name='create_suggestion'),
+    #url(r'^admin/', include(admin.site.urls)),
+
+)
